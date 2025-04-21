5702380a2ae2bcda9d16e43651ac332b1b8920cb
CoachSandeep <sandeep@untrain.fit>
2025-04-21 20:37:37 +0530

    Delete WOD.js

--- WOD.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const WODSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    duration: { type: String, required: true }
-});
-
-module.exports = mongoose.model('WOD', WODSchema);
